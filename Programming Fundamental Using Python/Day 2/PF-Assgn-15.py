d34002030acce9e48987b4198e04ec5ef97ab257
SUDARSHAN <34470884+SUDARSHANTADAGE@users.noreply.github.com>
2020-05-24 03:19:34 -0700

    Update PF-Assgn-15.py

--- Programming Fundamental Using Python/Day 2/PF-Assgn-15.py
+++ Programming Fundamental Using Python/Day 2/PF-Assgn-15.py
@@ -1,1 +1,18 @@
+#PF-Assgn-15
 
+def find_product(num1,num2,num3):
+    product=0
+    #write your logic here
+    if(num1!=7 and num2!=7 and num3!=7):
+        product=num3*num1*num2
+    elif(num1==7 and num2!=7 and num3!=7):
+        product=num3*num2
+    elif(num1!=7 and num2==7 and num3!=7):
+        product=num3
+    else:
+        product=-1
+    return product
+
+#Provide different values for num1, num2, num3 and test your program
+product=find_product(7,6,2)
+print(product)

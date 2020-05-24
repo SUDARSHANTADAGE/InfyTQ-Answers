9e5d583df1ecafe22a3bb5f6900fa8ba579d5307
SUDARSHAN <34470884+SUDARSHANTADAGE@users.noreply.github.com>
2020-05-24 02:56:45 -0700

    Create PF-Assgn-4.py

---
+++ Programming Fundamental Using Python/Day 1/PF-Assgn-4.py
@@ -0,0 +1,15 @@
+#PF-Assgn-4
+#This verification is based on string match.
+#Day 1
+principal=7800
+rate_of_interest=7.7
+time=26
+interest=0
+
+#Start writing your code from here
+#Populate the variable: interest
+interest=(principal*rate_of_interest*time)/100
+
+
+#Do not modify the below print statement for verification to work
+print(interest)

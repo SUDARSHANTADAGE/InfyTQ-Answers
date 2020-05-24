8f219dcd57688eceb9348ae5a2ecabf3d145744a
SUDARSHAN <34470884+SUDARSHANTADAGE@users.noreply.github.com>
2020-05-24 03:18:17 -0700

    Create PF-Exer-14.py

---
+++ Programming Fundamental Using Python/Day 2/PF-Exer-14.py
@@ -0,0 +1,14 @@
+#PF-Tryout
+#Start writing your code here
+score = 0
+
+if 80<=score<=100 :
+    print("A")
+elif 73<=score<=79 :
+    print("B")
+elif 65<=score<=72 :
+    print("C")
+elif 0<=score<=64 :
+    print("D")
+else:
+    print("Z")

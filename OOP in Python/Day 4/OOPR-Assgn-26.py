a8d1a3c76bce1a3693de5dc78f7d25bc68d6d2f8
SUDARSHAN <34470884+SUDARSHANTADAGE@users.noreply.github.com>
2020-08-28 18:45:25 +0530

    Create OOPR-Assgn-26.py

---
+++ OOP in Python/Day 4/OOPR-Assgn-26.py
@@ -0,0 +1,19 @@
+Class Book
+
+Attributes: author, name, code, price, year
+
+Methods:  __init__(author,name,code,price,year)
+          get_author_details()
+          
+          
+         
+ Class Author
+ 
+ Attributes: name, age, country
+ 
+ Methods: __init__(name,age,country)
+          write_books()
+          attend_press_meetings()
+          
+          
+Relationship : Book Has a Author
